c765094541d75148b37f6cab3486c650f242d069
Thiago Lyra <thiagolyra1@gmail.com>
2025-08-02 10:24:43 -0300

    teste2

---
+++ hackhaton_q2.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_classification
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
